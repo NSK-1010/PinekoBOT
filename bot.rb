7ea53cb2034be7f8bfcda3b510e78e41b34761e4
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-02-02 07:44:34 +0900

    Create bot.rb

--- bot.rb
+++ bot.rb
@@ -2,10 +2,14 @@ require 'discordrb'
 bot = Discordrb::Bot.new token: 'さっきメモったTokenをここに書く', client_id: メモったClient IDをここに書く , prefix: '!'
 
 bot.command :hello do |event|
-  event.respond 'おはこんばんじにちは!'
+  event.respond "おはこんばんじにちは!"
 end
 
-require 'uri'
-enc = URI.encode_www_form_component('Ruby リファレンスマニュアル')
+bot.command :dice do |event,word|
+  word = word.chomp
+  require 'uri'
+  enc = URI.encode_www_form_component(word)
+  event.respond "Googleで検索結果はこちらです。\n#{enc}"
+end
  
 bot.run
