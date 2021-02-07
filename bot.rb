ef55f0db0921ff224dcd96341691b72a98d0a795
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-02-07 21:21:31 +0900

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,14 +1,18 @@
 require 'discordrb'
-bot = Discordrb::Bot.new token: 'さっきメモったTokenをここに書く', client_id: メモったClient IDをここに書く , prefix: '!'
+bot = Discordrb::Bot.new token: 'さっきメモったTokenをここに書く', client_id: メモったClient IDをここに書く , prefix: '/'
+
+bot.command :help do |event|
+  event.respond "/help: ヘルプ\n/hello: あいさつしてくれるよ〜\n/ggrks 検索ワード: Google検索のページを開いてくれるよ〜"
+end
 
 bot.command :hello do |event|
   event.respond "おはこんばんじにちは!"
 end
 
-bot.command :dice do |event,word|
+bot.command :ggrks do |event,word|
   word = word.chomp
   require 'uri'
-  enc = URI.encode_www_form_component(word)
+  enc = "https://www.google.com/search?q=" + URI.encode_www_form_component(word)
   event.respond "Googleで検索結果はこちらです。\n#{enc}"
 end
  
